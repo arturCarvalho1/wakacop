84ffa255815bcf1b750072f8c7f988cbf3771c55
Artur Carvalho <arturpedroso5050@gmail.com>
2024-12-09 10:10:41 -0300

    init

---
+++ src/test/java/academy/Wakadanda/wakacop/WakacopApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.Wakadanda.wakacop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakacopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
